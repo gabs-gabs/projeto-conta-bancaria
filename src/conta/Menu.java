aa3bbced4df30214a93835b5577b285efb8f247e
Gabrielly <bii.souza018@gmail.com>
2025-03-31 22:51:25 -0300

    Criação conta corrente, conta poupança, interface ContaRepository e a classe Conta passa a ser abstrata

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -2,17 +2,25 @@ package conta;
 
 import java.util.Scanner;
 import conta.util.Cores;
-import conta.model.Conta;
+import conta.model.ContaCorrente;
+import conta.model.ContaPoupanca;
 
 public class Menu {
     public static void main(String[] args) {
 
-        Conta c1 = new Conta(1, 123, 1, "Adriana", 10000.0f);
-        c1.visualizar();
-        c1.sacar(12000.0f);
-        c1.visualizar();
-        c1.depositar(5000.0f);
-        c1.visualizar();
+        ContaCorrente cc1 = new ContaCorrente(1, 123, 1, "José da Silva", 0.0f, 1000.0f);
+        cc1.visualizar();
+        cc1.sacar(12000.0f);
+        cc1.visualizar();
+        cc1.depositar(5000.0f);
+        cc1.visualizar();
+
+        ContaPoupanca cp1 = new ContaPoupanca(2, 123, 2, "Maria dos Santos", 100000.0f, 15);
+        cp1.visualizar();
+        cp1.sacar(1000.0f);
+        cp1.visualizar();
+        cp1.depositar(5000.0f);
+        cp1.visualizar();
 
         Scanner leia = new Scanner(System.in);
         int opcao;
@@ -88,6 +96,7 @@ public class Menu {
         }
 
     }
+
     public static void sobre() {
         System.out.println("\n*********************************************************");
         System.out.println("Projeto Desenvolvido por: Gabrielly Vitória Fernanda de Souza");
